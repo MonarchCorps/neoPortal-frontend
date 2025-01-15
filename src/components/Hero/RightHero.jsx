4533ba4a09b361e4f9feddd13eddf7795acf009a
Monarch Corps <monarchcorps01@gmail.com>
2025-01-15 16:13:21 +0100

    Changed hero image to that of black people

--- src/components/Hero/RightHero.jsx
+++ src/components/Hero/RightHero.jsx
@@ -2,10 +2,13 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 import { Autoplay } from 'swiper/modules'
 import 'swiper/swiper-bundle.css'
 
-const heroImg1 = 'https://res.cloudinary.com/dh5a8opoe/image/upload/q_auto/f_auto/v1735378104/Exam_Portal/b67xpysoalro17rta9um.jpg'
+// const heroImg1 = 'https://res.cloudinary.com/dh5a8opoe/image/upload/q_auto/f_auto/v1735378104/Exam_Portal/b67xpysoalro17rta9um.jpg'
+const heroImg1 = 'https://res.cloudinary.com/dh5a8opoe/image/upload/f_auto,q_auto/v1/Exam_Portal/dhefpqcmwzrum1mcinag'
 const heroImg2 = 'https://res.cloudinary.com/dh5a8opoe/image/upload/q_auto/f_auto/v1735378142/Exam_Portal/em3v9eeyxrym85j0grbl.jpg'
-const heroImg3 = 'https://res.cloudinary.com/dh5a8opoe/image/upload/q_auto/f_auto/v1735378207/Exam_Portal/i0hquqivopj1f1nuxicm.jpg'
-const heroImg4 = 'https://res.cloudinary.com/dh5a8opoe/image/upload/q_auto/f_auto/v1735378254/Exam_Portal/zjc6l4h3tehqfrolzmzy.jpg'
+// const heroImg3 = 'https://res.cloudinary.com/dh5a8opoe/image/upload/q_auto/f_auto/v1735378207/Exam_Portal/i0hquqivopj1f1nuxicm.jpg'
+const heroImg3 = 'https://res.cloudinary.com/dh5a8opoe/image/upload/f_auto,q_auto/v1/Exam_Portal/oev6muqjzcyvxxqlylan'
+// const heroImg4 = 'https://res.cloudinary.com/dh5a8opoe/image/upload/q_auto/f_auto/v1735378254/Exam_Portal/zjc6l4h3tehqfrolzmzy.jpg'
+const heroImg4 = 'https://res.cloudinary.com/dh5a8opoe/image/upload/f_auto,q_auto/v1/Exam_Portal/isw1m68np15c1skojjlg'
 
 function RightHero() {
 
@@ -31,7 +34,7 @@ function RightHero() {
                         <img
                             src={content.image}
                             alt="Hero Image"
-                            className='size-full object-cover rounded-md h-full min-h-[32rem] hrmd:h-full'
+                            className='size-full object-cover rounded-md h-full min-h-[32rem] max-h-[32rem] hrmd:h-full'
                             loading='eager'
                         />
                     </SwiperSlide>
