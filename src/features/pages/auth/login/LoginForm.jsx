b821a291f0987a6800ea6faa7b45bc541b834cd8
Monarch Corps <monarchcorps01@gmail.com>
2025-01-16 09:50:42 +0100

    Added a text on the login page

--- src/features/pages/auth/login/LoginForm.jsx
+++ src/features/pages/auth/login/LoginForm.jsx
@@ -8,6 +8,12 @@ function LoginForm({ formData, handleChange, handleLogin }) {
     return (
         <>
             <form className="size-full grid grid-cols-1 h-fit items-center justify-center max-w-[30rem] mx-auto mt-20 p-4 gap-y-1">
+                <h1 className='col-span-full mb-5 text-3xl font-serif cfsm:text-2xl'>
+                    Login to your account
+                    <span className='ml-2 text-sm align-middle'>
+                        <span className='bg-[#1F509A] p-1 text-[#fff]'>N</span><span className='text-[#fff] p-1 bg-slate-900'> P</span>
+                    </span>
+                </h1>
                 <div className='flex flex-col w-full mb-4'>
                     <label className='text-sm mb-1 font-500' htmlFor="state">Enter name or email</label>
                     <input
